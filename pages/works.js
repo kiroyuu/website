c920e977de6d6c2d0ab83dafe59a8b489cf873a0
joalhelk <joalhelk@jyu.fi>
2023-01-12 21:42:01 +0200

    feat(*): works templates

--- pages/works.js
+++ pages/works.js
@@ -1,4 +1,7 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
+import Section from "../components/section";
+import { WorkGridItem } from "../components/grid-item";
+import thumbWatcher from "../public/images/watcher.png";
 
 const Works = () => {
   return (
@@ -6,6 +9,15 @@ const Works = () => {
       <Heading as="h3" fontSize={20} mb={4}>
         Works
       </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem id="watcher" title="Watcher" thumbnail={thumbWatcher}>
+            A desktop application to monitor your IoT devices and their sensors.
+            Made with Java, Spring Boot, and PostgreSQL.
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
     </Container>
   );
 };
